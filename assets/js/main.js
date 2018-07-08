d00a81aeff34e6a8de9966c5c720c5fd2bbcf190
aleksey <maylo.kondratev@yandex.ru>
2018-07-08 23:33:47 +0300

    Init

---
+++ assets/js/main.js
@@ -0,0 +1,53 @@
+// $(function() {
+
+// 	$('#cover-slider').slick({
+// 	  slidesToShow: 1,
+// 	  slidesToScroll: 1,
+// 		infinity: true,
+// 	  arrows: false,
+// 	  fade: true,
+// 	  asNavFor: '#main-slider'
+// 	});
+
+// 	$('#main-slider').slick({
+// 		dots: true,
+// 	  slidesToShow: 1,
+// 	  slidesToScroll: 1,
+// 		infinity: true,
+// 	  arrows: true,
+// 		focusOnSelect: false,
+// 		asNavFor: '#cover-slider',
+// 		prevArrow: '.slider__arrow_prev',
+// 		nextArrow: '.slider__arrow_next'
+// 	});
+
+
+// });
+
+
+
+//  let vm = new Vue({
+//  	el: '#app',
+//  	data: function() {
+//  		return {
+//  			list: [
+//                 {
+//                     id: 1,
+//                     name: 'Name 1'
+//                 }
+//             ]
+//  		}
+//  	}
+//  });
+
+var txt = `Привет!
+					[Тестовый режим; нажми Ctrl+D to save and quit; нажми Ctrl+C to quit without saving]
+
+					###команды
+
+					- run
+					- refresh
+					- destroy
+					- optimize ?`;
+
+// var tw = new TypeWriter('#typewriter', { outTxt: txt });
