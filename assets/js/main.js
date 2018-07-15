40883040e38725e5ec5b3c9124bef49ec2dfcb95
aleksey <maylo.kondratev@yandex.ru>
2018-07-16 00:40:36 +0300

    Калькулятор и не только

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,53 +1,55 @@
-// $(function() {
+var calculator = new Calculator('#calculator');
 
-// 	$('#cover-slider').slick({
-// 	  slidesToShow: 1,
-// 	  slidesToScroll: 1,
-// 		infinity: true,
-// 	  arrows: false,
-// 	  fade: true,
-// 	  asNavFor: '#main-slider'
-// 	});
+$(function() {
+	$('#summa').rangeslider({
+		polyfill: false,
+		onInit: function() {
+			this.value = calculator._params.summa;
+		},
+		onSlide: function(position, value) {
+			calculator.updateInputValue(calculator.summaCtrl, value);
+			calculator.calculate();
+		},
+		onSlideEnd: function(position, value) {}
+	});
 
-// 	$('#main-slider').slick({
-// 		dots: true,
-// 	  slidesToShow: 1,
-// 	  slidesToScroll: 1,
-// 		infinity: true,
-// 	  arrows: true,
-// 		focusOnSelect: false,
-// 		asNavFor: '#cover-slider',
-// 		prevArrow: '.slider__arrow_prev',
-// 		nextArrow: '.slider__arrow_next'
-// 	});
+	$('#refil').rangeslider({
+		polyfill: false,
+		onInit: function() {
+			this.value = calculator._params.refil;
+		},
+		onSlide: function(position, value) {
+			calculator.updateInputValue(calculator.refilCtrl, value);
+			calculator.calculate();
+		},
+		onSlideEnd: function(position, value) {}
+	});
 
+	$('.js-callback').on('click', function() {
+		$('.md').addClass('_show');
+	});
 
-// });
+	$('.js-md-close').on('click', function() {
+		$(this).parent().parent().parent().removeClass('_show');
+	});
 
+	$('.md__overlay').on('click', function() {
+		$(this).parent().removeClass('_show');
+	});
+});
 
-
-//  let vm = new Vue({
-//  	el: '#app',
-//  	data: function() {
-//  		return {
-//  			list: [
-//                 {
-//                     id: 1,
-//                     name: 'Name 1'
-//                 }
-//             ]
-//  		}
-//  	}
-//  });
-
-var txt = `Привет!
-					[Тестовый режим; нажми Ctrl+D to save and quit; нажми Ctrl+C to quit without saving]
-
-					###команды
-
-					- run
-					- refresh
-					- destroy
-					- optimize ?`;
-
-// var tw = new TypeWriter('#typewriter', { outTxt: txt });
+var myMap, myPlacemark;
+ymaps.ready(initMap);
+function initMap() { 
+	myMap = new ymaps.Map("map", {
+		center: [55.76, 37.64],
+		zoom: 13,
+		controls: ['zoomControl', 'fullscreenControl']
+	});
+	myPlacemark = new ymaps.Placemark([55.76, 37.64], {
+		hintContent: 'Подсказка',
+		balloonContent: 'Содержимое метки'
+	});
+	myMap.geoObjects.add(myPlacemark);
+	myMap.behaviors.disable('scrollZoom');
+};
